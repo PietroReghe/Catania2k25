87f5b4a5c3a8c648de48022de905d50472c0e835
PietroReghe <pietro.reghenzi.jr@gmail.com>
2025-03-05 17:34:03 +0100

    prova seriale

---
+++ provaseriale.py
@@ -0,0 +1,20 @@
+
+#! /home/pietro/myenv/bin/python
+
+
+import serial
+import time
+
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        ser.write(b"HELLO ARDUINO!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print("Input", line, '\n')
+
+        time.sleep(1)
+
+
+
