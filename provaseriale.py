55eae1703c51a4ac50b2ccaf0e50c8fbd123c179
PietroReghe <pietro.reghenzi.jr@gmail.com>
2025-03-06 16:42:09 +0100

    serialemodificata

--- provaseriale.py
+++ provaseriale.py
@@ -9,12 +9,13 @@ import time
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
     ser.reset_input_buffer()
-    while True:
-        ser.write(b"HELLO ARDUINO!\n")
-        line = ser.readline().decode('utf-8').rstrip()
-        print("Input", line, '\n')
-
-        time.sleep(1)
+    ser.write(b"HELLO ARDUINO!\n")
+    line = ser.readline().decode('utf-8').rstrip()
+    print("Input", line, '\n')
+    time.sleep(1)
+    
+        
+        
 
 
 
