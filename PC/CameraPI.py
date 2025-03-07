c8db360d9d3e1294abe248f79d6c8eeb8ebfa9e5
PietroReghe <pietro.reghenzi.jr@gmail.com>
2025-03-07 14:46:11 +0100

    speriamo.funzioni

--- PC/CameraPI.py
+++ PC/CameraPI.py
@@ -9,12 +9,13 @@ import cv2
 
 
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
 
 
 def read_camera_pi():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     while(True):
         # Start a while loop 
